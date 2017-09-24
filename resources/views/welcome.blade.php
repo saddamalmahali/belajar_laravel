0532df8184d4e515fef4b55d2695cdf6f533b353
Saddam Almahali <saddam.almahali@gmail.com>
2017-09-25 04:41:23 +0700

    edit tulisan di welcomepage.blade 2

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Belajar Laravel
+                    Belajar Laravel 2
                 </div>
 
             </div>
