dd483af994b307512c73ecbd167dca454ad60041
Saddam Almahali <saddam.almahali@gmail.com>
2017-09-25 04:56:00 +0700

    tambah Route untuk rizaldi dan depi

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,10 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/rijaldi', function(){
+    return view('rijaldi');
+});
+Route::get('/depi', function(){
+    return view('depi');
+});
